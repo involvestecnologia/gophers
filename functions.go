45eabbd3b352b0932492cf1dad1eb408ae106c24
Thiago Trennepohl <thiagotrennepohl@gmail.com>
2018-03-23 18:35:45 -0300

    First commit

---
+++ functions.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func imprimeNome(algumnome string) {
+	fmt.Println(algumnome)
+}
---
+++ helloworld.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+var numero int = 10
+var nome string = "gopher"
+var golangEManeiro = true
+
+func main() {
+	fmt.Println("Hello World")
+
+	fmt.Println(numero)
+	fmt.Println(nome)
+	fmt.Println(golangEManeiro)
+}
